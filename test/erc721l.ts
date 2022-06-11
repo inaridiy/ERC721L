aa7442d6018a524c96fadfb578d1e9e3c8f7cbe7
inaridiy <inainainariinainari@gmail.com>
2022-06-11 22:56:54 +0900

    カバレッジ100%

--- test/erc721l.ts
+++ test/erc721l.ts
@@ -1,4 +1,5 @@
 import { expect } from "chai";
+import { ethers } from "hardhat";
 import {
   deployContract,
   getSigners,
@@ -6,6 +7,7 @@ import {
   BASE_PERIOD,
   jumpTime,
   MARGIN,
+  ZERO,
 } from "./utils";
 
 describe("ERC721Loanable", function () {
@@ -43,8 +45,98 @@ describe("ERC721Loanable", function () {
     } = await getSigners();
     await instance.connect(addr1).mint();
     await instance.connect(addr1).lend(addr2.address, 0, BASE_PERIOD);
-    jumpTime(BASE_PERIOD.add(MARGIN));
+    await jumpTime(BASE_PERIOD.add(MARGIN));
     await instance.connect(addr1).reclaim(0);
     expect(await instance.ownerOf(0)).to.eq(addr1.address);
+  });
+
+  it("testing lend by borrower", async () => {
+    const instance = await deployContract();
+    const {
+      addrs: [addr1, addr2, addr3],
+    } = await getSigners();
+    await instance.connect(addr1).mint();
+    await instance.connect(addr1).lend(addr2.address, 0, BASE_PERIOD);
+    await expect(
+      instance.connect(addr2).lend(addr3.address, 0, BASE_PERIOD)
+    ).to.revertedWith("ERC721Loanable: lend caller is not true owner");
+  });
+
+  it("testing lend to owner", async () => {
+    const instance = await deployContract();
+    const {
+      addrs: [addr1],
+    } = await getSigners();
+    await instance.connect(addr1).mint();
+    await expect(
+      instance.connect(addr1).lend(addr1.address, 0, BASE_PERIOD)
+    ).to.revertedWith("ERC721Loanable: Cannot lend to itself.");
+  });
+
+  it("testing reclaim by not true owner", async () => {
+    const instance = await deployContract();
+    const {
+      addrs: [addr1, addr2, addr3],
+    } = await getSigners();
+    await instance.connect(addr1).mint();
+    await instance.connect(addr1).lend(addr2.address, 0, BASE_PERIOD);
+    await jumpTime(BASE_PERIOD.add(MARGIN));
+    await expect(instance.connect(addr3).reclaim(0)).to.revertedWith(
+      "ERC721Loanable: reclaim caller is not true owner"
+    );
+  });
+
+  it("testing reclaim in deadline", async () => {
+    const instance = await deployContract();
+    const {
+      addrs: [addr1, addr2],
+    } = await getSigners();
+    await instance.connect(addr1).mint();
+    await instance.connect(addr1).lend(addr2.address, 0, BASE_PERIOD);
+
+    await expect(instance.connect(addr1).reclaim(0)).to.revertedWith(
+      "ERC721Loanable: token is during the lease period"
+    );
+  });
+
+  it("testing reclaim in deadline", async () => {
+    const instance = await deployContract();
+    const {
+      addrs: [addr1, addr2],
+    } = await getSigners();
+    await instance.connect(addr1).mint();
+    await instance.connect(addr1).lend(addr2.address, 0, BASE_PERIOD);
+
+    await expect(instance.connect(addr1).reclaim(0)).to.revertedWith(
+      "ERC721Loanable: token is during the lease period"
+    );
+  });
+
+  it("testing transfer by borrower", async () => {
+    const instance = await deployContract();
+    const {
+      addrs: [addr1, addr2, addr3],
+    } = await getSigners();
+    await instance.connect(addr1).mint();
+    await instance.connect(addr1).lend(addr2.address, 0, BASE_PERIOD);
+    await expect(
+      instance.connect(addr2).transferFrom(addr2.address, addr3.address, 0)
+    ).to.revertedWith("ERC721Loanable: transfer caller is not true owner");
+  });
+
+  it("testing get lending info", async () => {
+    const instance = await deployContract();
+    const {
+      addrs: [addr1, addr2, addr3],
+    } = await getSigners();
+    await instance.connect(addr1).mint();
+    await instance.connect(addr1).lend(addr2.address, 0, BASE_PERIOD);
+    const { timestamp } = await ethers.provider.getBlock("latest");
+    expect(await instance.lendingInfo(0)).to.eql([
+      addr1.address,
+      addr2.address,
+      ZERO,
+      BASE_PERIOD.add(timestamp),
+    ]);
   });
 });
